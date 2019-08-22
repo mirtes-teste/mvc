8824b3629e79966fc025b3eaacf4378df0e5eee4
Renan Lima <renan.arantes123@gmail.com>
2019-08-22 15:57:24 -0300

    Refatorando e abstraindo rotas

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -6,6 +6,11 @@ namespace App\Controllers;
 class Index
 {
 	
+	public function index()
+	{
+		echo "Controller: Index, Action: Index";
+	}
+
 	public function empresa()
 	{
 		echo "Controller: Index, Action: Empresa";
--- App/Init.php
+++ App/Init.php
@@ -2,50 +2,18 @@
 
 namespace App;
 
-class Init
-{
-
-	private $routes;
-
-	public function __construct()
-	{
-		$this->initRoutes();
-		$this->run($this->getUrl());
-	}
+use SON\Init\Bootstrap;
 
 
 
-	public function initRoutes()
+class Init extends Bootstrap
+{	
+
+	protected function initRoutes()
 	{
 		$ar['home'] = array('route' => '/', 'controller' => 'index', 'action' => 'index');
 		$ar['empresa'] = array('route' => '/empresa', 'controller' => 'index', 'action' => 'empresa');
 		$this->setRoutes($ar);
 	}
 
-	public function run($url)
-	{
-		array_walk($this->routes, function($route) use($url){
-			
-			if($url == $route['route']){
-				$class = "App\\Controllers\\".ucfirst($route['controller']);
-				$action = $route['action'];
-        		$controller = new $class;
-        		$controller->$action();
-			}
-		});
-	}
-
-	public function setRoutes(array $routes)
-	{
-		$this->routes = $routes;
-	}
-
-	public function getUrl()
-	{
-		return parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
-	}
-
 }
-
-
-
