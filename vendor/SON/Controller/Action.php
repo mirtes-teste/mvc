3a6c8a33711e1b8338cd4cabdcfe336c1515689d
Renan Lima <renan.arantes123@gmail.com>
2019-08-23 11:55:41 -0300

    renderizando layout

--- vendor/SON/Controller/Action.php
+++ vendor/SON/Controller/Action.php
@@ -5,18 +5,30 @@ namespace SON\Controller;
 class Action
 {
 	protected $view;
+	protected $action;
 
 	public function __construct()
 	{
 		$this->view = new \stdClass;
 	}
 
-	public function render($action)
+	public function render($action, $layout = true)
+	{
+		$this->action = $action;
+		if($layout == true && file_exists("../App/views/layout.phtml")){
+			include_once '../App/views/layout.phtml';
+		}
+
+		else {
+			$this->content();
+		}
+	}
+
+	public function content()
 	{
 		$atual = get_class($this);
 		$singleClassName = strtolower(str_replace("App\\Controllers\\", "", $atual));
-		include_once '../App/views/'.$singleClassName.'/'.$action.'.phtml';
-
+		include_once '../App/views/'.$singleClassName.'/'.$this->action.'.phtml';
 	}
 
 }
