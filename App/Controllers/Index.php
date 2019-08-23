4c918a4f31fc78b8337d5cb3b73f9814537bab66
Renan Lima <renan.arantes123@gmail.com>
2019-08-23 11:21:27 -0300

    entendendo de variaveis

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -5,9 +5,21 @@ namespace App\Controllers;
 
 class Index
 {
+	private $view;
+
+	public function __construct()
+	{
+		$this->view = new \stdClass;
+	}
 	
 	public function index()
 	{
+		$this->view->x = 1;
+		$nomes = array();
+		$nomes[] = 'Wesley';
+		$nomes[] = 'João';
+
+		$this->view->nomes = $nomes;
 		include '../App/views/index/index.phtml';
 	}
 
